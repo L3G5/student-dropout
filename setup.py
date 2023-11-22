3ceacfc0a1de41f7a385b0514b5a648a1bcaef89
Le <nickolajjt@gmail.com>
2023-11-22 06:58:10 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='opout (or rather exploring the dataset)',
+    author='le_g3',
+    license='BSD-3',
+)
